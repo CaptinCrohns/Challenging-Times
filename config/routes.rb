8f97ba93aeeeef15be35eb15fb93662c54e43c7b
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-10-27 17:18:40 +0000

    First commit which included creating app, controller pages with a home and about, editing routing and created menu for application layout

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: 'pages#home', as: 'home'
+
+  get 'about_us', to: 'pages#about', as: 'about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
