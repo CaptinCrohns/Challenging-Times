d6a6c387e9bb75c5321eec0a8b2d20f8e302ff7c
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-10-29 17:28:07 +0000

    Added faker gem to gemfile and generated 100 entries, and ran db:seed

--- app/models/how_i_met_your_mother.rb
+++ app/models/how_i_met_your_mother.rb
@@ -1,7 +1,5 @@
 class HowIMetYourMother < ApplicationRecord
-  validates :character, uniqueness: true
+  validates :character, length: {minimum: 3}
   validates :character, presence: true
   validates :catch_phrase, presence: true
-
-
 end
